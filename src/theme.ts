dc3a1054ef5f88b604425f2d02d3cbc85b0b5e43
Latif Essam <latifekhaliel@gmail.com>
2023-09-19 22:34:07 +0300

    Customize Theme:dark gray

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#F8F9FA",
+      100: "#E9ECEF",
+      200: "#DEE2E6",
+      300: "#CED4DA",
+      400: "#ADB5BD",
+      500: "#6C757D",
+      600: "#495057",
+      700: "#343A40",
+      800: "#212529",
+      900: "#000",
+    },
+  },
+});
 
 export default theme;
