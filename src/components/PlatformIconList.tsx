5ba500c65555dc68af253b0be91e94dbb8663d02
Latif Essam <latifekhaliel@gmail.com>
2023-09-19 22:37:05 +0300

    Fix import typo on PlatformIconList

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,9 +11,9 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 
-import { Platform } from "../hooks/useGames";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import { Platform } from "../hooks/usePlatforms";
 
 interface PlatformIconListProps {
   platforms: { platform: Platform }[];
